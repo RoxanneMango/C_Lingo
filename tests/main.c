39758b1bc678e351ecf7fc7f8ab4a52b84cec65f
RoxanneMango <roxannevdpol@gmail.com>
2020-09-08 19:30:34 +0200

    ++black terminal background when testing

--- tests/main.c
+++ tests/main.c
@@ -17,6 +17,8 @@ main(int argc, char ** argv)
 	int number_of_suites = 3;
 	int suites_passed = 0;
 
+	printf("\033[48;2;0;0;0m");
+
 	net_test(is_remote) ? ++suites_passed : 0;
 	printf("\n");
 	file_test() ? ++suites_passed : 0;
@@ -24,6 +26,8 @@ main(int argc, char ** argv)
 	mem_test() ? ++suites_passed : 0;
 	printf("\n");
 	printf("\033[1;30m--\nNumber of passed suites: %s%d/%d\033[1;30m\n--\n\033[0m", (suites_passed == number_of_suites ? "\033[1;32m" : "\033[1;31m"), suites_passed, number_of_suites);
+
+	printf("\033[0");
 
 	return ((suites_passed == number_of_suites) ? 0 : 1);
 }
